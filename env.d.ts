90ff678182e309fcaa1427a709869d0724c15e2f
Geslain Dahan <geslain.dahan@spaceable.org>
2024-05-09 20:09:37 +0545

    chore(Database): add database connection

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace NodeJS {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ProcessEnv {
+    DATABASE_HOST: string;
+    DATABASE_PORT: number;
+    DATABASE_USERNAME: string;
+    DATABASE_PASSWORD: string;
+    DATABASE_NAME: string;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+    UsersModule,
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
