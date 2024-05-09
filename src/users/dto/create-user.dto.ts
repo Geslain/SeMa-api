9b112e79d37948584a023b7236cb10976e0645ae
Geslain Dahan <geslain.dahan@spaceable.org>
2024-05-09 20:52:22 +0545

    feat(User): add user feature
    Add nest generated resource and update model and service

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+const passwordRegEx =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: 'Name must have at least 2 characters.' })
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsString()
+  @MinLength(2, { message: 'Name must have at least 2 characters.' })
+  @IsNotEmpty()
+  lastname: string;
+
+  @IsNotEmpty()
+  @IsEmail(null, { message: 'Please provide valid Email.' })
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(passwordRegEx, {
+    message: `Password must contain Minimum 8 and maximum 20 characters, 
+    at least one uppercase letter, 
+    one lowercase letter, 
+    one number and 
+    one special character`,
+  })
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user: User = new User();
+
+    user.firstname = createUserDto.firstname;
+    user.lastname = createUserDto.lastname;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+
+    return this.userRepository.save(user);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const user: User = new User();
+
+    user.firstname = updateUserDto.firstname;
+    user.lastname = updateUserDto.lastname;
+    user.email = updateUserDto.email;
+    user.password = updateUserDto.password;
+    user.id = id;
+
+    return this.userRepository.save(user);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
