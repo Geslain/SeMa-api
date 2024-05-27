fab99b96f8a92af32bff402f4d4880891872ccfd
Geslain Dahan <geslain.dahan@spaceable.org>
2024-05-27 15:32:38 +0545

    feat(user factories): update factory to randomize password

--- src/users/factories/user.factory.ts
+++ src/users/factories/user.factory.ts
@@ -4,20 +4,20 @@ import { Factory } from 'fishery';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
 
-export const userFactory = Factory.define<User>(() => ({
-  id: faker.string.uuid(),
-  firstname: faker.person.firstName(),
-  lastname: faker.person.lastName(),
-  email: faker.internet.email(),
-  password: 'a123!eGsl',
-  fields: [],
-  created_at: faker.date.past(),
-  updated_at: faker.date.recent(),
-}));
-
 export const userDtoFactory = Factory.define<CreateUserDto>(() => ({
   firstname: faker.person.firstName(),
   lastname: faker.person.lastName(),
   email: faker.internet.email(),
-  password: 'a123!eGsl',
+  password: faker.internet.password({
+    length: 20,
+    pattern: /[A-Za-z\d@$!%*?&]/,
+  }),
+}));
+
+export const userFactory = Factory.define<User>(() => ({
+  id: faker.string.uuid(),
+  fields: [],
+  created_at: faker.date.past(),
+  updated_at: faker.date.recent(),
+  ...userDtoFactory.build(),
 }));
